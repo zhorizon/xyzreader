9b40bced8a0aa42ca1f79511c5dae056b7a632a0
Cencil <cencil@gmail.com>
2016-08-14 17:07:52 +0800

    Add image finished load fade-in

--- XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
@@ -1,11 +1,16 @@
 package com.example.xyzreader.ui;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.AttributeSet;
 
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
 
 public class DynamicHeightNetworkImageView extends NetworkImageView {
+    private static final int ANIM_DURATION = 500;
+    private boolean mShouldAnimate = false;
     private float mAspectRatio = 1.5f;
 
     public DynamicHeightNetworkImageView(Context context) {
@@ -30,5 +35,19 @@ public class DynamicHeightNetworkImageView extends NetworkImageView {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int measuredWidth = getMeasuredWidth();
         setMeasuredDimension(measuredWidth, (int) (measuredWidth / mAspectRatio));
+    }
+
+    @Override
+    public void setImageBitmap(Bitmap bm) {
+        super.setImageBitmap(bm);
+        if (mShouldAnimate) {
+            ObjectAnimator.ofFloat(this, "alpha", 0, 1).setDuration(ANIM_DURATION).start();
+        }
+    }
+
+    @Override
+    public void setImageUrl(String url, ImageLoader imageLoader) {
+        mShouldAnimate = !imageLoader.isCached(url, 0, 0);
+        super.setImageUrl(url, imageLoader);
     }
 }
